89caf4af1299abf5867edef3f0510cdd3f9ac861
Brittany Morris <brittmmendez@gmail.com>
2018-01-12 14:09:22 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -26,6 +26,24 @@ def self.drop_table
   DB[:conn].execute(sql)
 end
 
+def save
+  if self.id
+    self.update
+  else
+    sql =  <<-SQL
+    INSERT INTO dogs (name, breed) VALUES (?, ?)
+  SQL
+  DB[:conn].execute(sql, self.name, self.breed)
+  @id=DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+end
+end
+
+def self.create(name:, breed:)
+  dog=Dog.new(name,breed)
+  dog.save
+  dog  
+end
+
 def self.new_from_db(row)
   id=row[0]
   name=row[1]
@@ -57,22 +75,5 @@ def update
   DB[:conn].execute(sql, self.name, self.breed, self.id)
 
 end
-def save
-  if self.id
-    self.update
-  else
-    sql =  <<-SQL
-    INSERT INTO dogs (name,breed) VALUES (?,?)
-  SQL
-  DB[:conn].execute(sql, self.name, self.breed)
-  @id=DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
-end
-end
-
-
-
-
-
-
 
 end
