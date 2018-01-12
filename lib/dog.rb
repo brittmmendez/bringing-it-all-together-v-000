184c9c0c74fd617d356502142e4ec22d68cf6ac5
Brittany Morris <brittmmendez@gmail.com>
2018-01-12 14:24:52 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -87,7 +87,7 @@ def update
 end
 
 def self.find_or_create_by(name:,breed:)
-  dog=DB[:conn].execute("SELECT * FROM dogs WHERE name=? AND breed=?, name, breed")
+  dog=DB[:conn].execute("SELECT * FROM dogs WHERE name=? AND breed=?", name, breed)
 
   if !dog.empty?
     dog_data=dog[0]
