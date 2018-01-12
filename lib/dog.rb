494affb1dfd9b9ee3aae4c33ce5641a8b1784cde
Brittany Morris <brittmmendez@gmail.com>
2018-01-12 13:57:55 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -34,7 +34,19 @@ def self.new_from_db(row)
   dog
 end
 
+def self.find_by_name(name)
+  sql=<<-SQL
+  SELECT * FROM dogs
+  WHERE name=?
+  LIMIT 1 
+  SQL
 
+  DB[:conn].execute(sql, name).map do |row|
+    self.new_from_db(row)
+  end.first
+end
+  
+end
 def save
   if self.id
     self.update
